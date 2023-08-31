c5d4f0db565113f53bf859baaa6506c3a27f1abd
Semilael <ariana-ses97@yandex.ru>
2023-08-31 22:38:37 +0200

    Cockatoo app

---
+++ index.js
@@ -0,0 +1,81 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-cart-list-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
+    
+    clearInputFieldEl()
+})
+
+onValue(shoppingListInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+    
+        clearShoppingListEl()
+        
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            
+            appendItemToShoppingListEl(currentItem)
+        }    
+    } else {
+        shoppingListEl.innerHTML = "No items here... yet"
+    }
+})
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+}
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+    
+    let newEl = document.createElement("li")
+    newEl.textContent = itemValue
+    
+    let checkIcon = document.createElement("span");
+    checkIcon.innerHTML = "✓";
+    checkIcon.className = "check-icon";
+
+    let deleteIcon = document.createElement("span");
+    deleteIcon.innerHTML = "✕";
+    deleteIcon.className = "delete-icon";
+
+    newEl.appendChild(checkIcon);
+    newEl.appendChild(deleteIcon);
+
+    newEl.addEventListener("click", function(event) {
+        if (event.target === checkIcon) {
+            newEl.classList.toggle("checked");
+        } else if (event.target === deleteIcon) {
+            let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`);
+            remove(exactLocationOfItemInDB);
+        }
+    });
+    // newEl.addEventListener("click", function() {
+    //     let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+        
+    //     remove(exactLocationOfItemInDB)
+    // })
+    
+    shoppingListEl.append(newEl)
+}
